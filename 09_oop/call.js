c5687dc5dfea47ee328b400ac2fb983add28f5cf
hellosamyak <jainsamyak0805@gmail.com>
2026-05-24 00:27:57 +0530

    add call.js with user creation functions

---
+++ 09_oop/call.js
@@ -0,0 +1,12 @@
+function createUsername(username) {
+  this.username = username
+}
+
+function createUser(username, email, password) {
+  createUsername.call(this, username) // call method & using this keyword here to hold the context of the prev method, even when it goes out of the execution context
+  this.email = email
+  this.password = password
+}
+
+const blacked = new createUser('blacked', 'blckd@racism.com', '@3#4')
+console.log(blacked)
